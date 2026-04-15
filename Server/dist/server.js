e749101086c1858e98753bf18f3ab9c1cb0f2877
Khulyso john <khuliso.thavhiwa@techbridlefoundation.org>
2026-04-15 13:43:06 +0200

    db configured

--- Server/dist/server.js
+++ Server/dist/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -50,10 +41,10 @@ app.use(errorHandler_1.notFound); // ✅ important before errorHandler
 // global error handler
 app.use(errorHandler_1.errorHandler);
 // connect to database and start server
-const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+const startServer = async () => {
     //console.log("Starting server...");
     try {
-        yield (0, database_1.default)();
+        await (0, database_1.default)();
         logger_1.default.info('Database connection successful, starting server...');
         app.listen(PORT, () => {
             logger_1.default.info(`Server is running on port ${PORT}`);
@@ -64,5 +55,5 @@ const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
         logger_1.default.error('Failed to start server', error);
         process.exit(1); // ✅ fail fast if DB fails
     }
-});
+};
 startServer();
